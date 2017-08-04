e5304fa45fe9c885c8c54d42e706ee0dcc276173
ALFA <r.muslimov.neutrino@yandex.ru>
2017-08-04 19:01:12 +0300

    Table style fixes. Artefacts removed

--- CustomControl/Services/TimeIntervalsService.cs
+++ CustomControl/Services/TimeIntervalsService.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml;
 using System.Xml.Serialization;
 using CustomControl.Models;
-using Microsoft.Ajax.Utilities;
 
 namespace CustomControl.Services
 {
